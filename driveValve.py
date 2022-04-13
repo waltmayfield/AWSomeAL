74659ee6caecdd6d293f3327a996234c353b4115
Walt Mayfield <waltmayf@amazon.com>
2022-04-13 23:14:11 +0100

    Changed cert location

--- driveValve.py
+++ driveValve.py
@@ -2,26 +2,27 @@ from gpiozero import OutputDevice
 import threading
 from random import random
 
-MQTT_VALVE_STATE_TOPIC = 'RPi/ValveState'
+#MQTT_VALVE_STATE_TOPIC = 'RPi/ValveState'
 
 class controlValve:
-	def __init__(self, openPin,closePin,onTime,offTime,mqttManager):
+	def __init__(self, openPin,closePin,onTime,offTime,mqttManager,mqttTopic):
 		self.openPin = OutputDevice(openPin)
 		self.closePin = OutputDevice(closePin)
 		self.onTime = onTime
 		self.offTime = offTime
 		self.mqttManager = mqttManager
+		self.mqttTopic = mqttTopic
 
 	def openValveStartCycle(self):
 		threading.Timer(self.onTime, self.closeValve).start()
 		self.closePin.off()
 		self.openPin.on()
-		self.mqttManager.send_msg(topic=MQTT_VALVE_STATE_TOPIC, msg={"ValveOpen" : True})
+		self.mqttManager.send_msg(topic=self.mqttTopic, msg={"ValveOpen" : True})
 
 	def closeValve(self):
 		threading.Timer(self.offTime, self.openValveStartCycle).start()
 		self.openPin.off()
 		self.closePin.on()
-		self.mqttManager.send_msg(topic=MQTT_VALVE_STATE_TOPIC, msg={"ValveOpen" : False})
+		self.mqttManager.send_msg(topic=self.mqttTopic, msg={"ValveOpen" : False})
 
 		
--- main.py
+++ main.py
@@ -1,21 +1,25 @@
+#!/usr/bin/env python
 from measureInputs import measureInputs
 from driveValve import controlValve
 from mqttManager import MQTTManager
 from uuid import uuid4
 import time
 import threading
+import os
 
-MQTT_SENSOR_MEASUREMENT_TOPIC = 'RPi/Data'
+MQTT_SENSOR_MEASUREMENT_TOPIC = 'RPi/Test'
+MQTT_VALVE_STATE_TOPIC = 'RPi/Test'
 
 measurmentPeriod = 0.2 
 valveOnTime = 2
 valveOffTime = 45
 
+certFolder = '/home/pi/certificates/'
 #https://github.com/SmartDogHouse/SmartDogHouse-SurveillanceSystem/tree/cf14788c53f9c7a884ae9cc3e5f0839fdd18713c/src/main/python
 mqttManager = MQTTManager(
-	cert_path = '/home/pi/Desktop/RasberryPi-1.cert.pem',
-	key_path = '/home/pi/Desktop/RasberryPi-1.private.key',
-	root_path = '/home/pi/Desktop/root-CA.crt',
+	cert_path = os.path.join(certFolder,'b7bea54d68aaa63204da17087aa3d7363d45f6dc76a29b0faf30dea3101daffc-certificate.pem.crt'),#'/home/pi/Desktop/RasberryPi-1.cert.pem',
+	key_path = os.path.join(certFolder,'b7bea54d68aaa63204da17087aa3d7363d45f6dc76a29b0faf30dea3101daffc-private.pem.key'),#'/home/pi/Desktop/RasberryPi-1.private.key',
+	root_path = os.path.join(certFolder,'AmazonRootCA1.pem'),#'/home/pi/Desktop/root-CA.crt',
 	port = 8883,
 	client_id = 'basicPubSub',
         server = 'a2upkalo7i6cik-ats.iot.us-east-1.amazonaws.com'
@@ -32,7 +36,7 @@ def publishSensorMeasurment():
 	mqttManager.send_msg(topic=MQTT_SENSOR_MEASUREMENT_TOPIC, msg=measureInputs())
 
 
-controlValve = controlValve(24,23,valveOnTime,valveOffTime,mqttManager)
+controlValve = controlValve(24,23,valveOnTime,valveOffTime,mqttManager,MQTT_VALVE_STATE_TOPIC)
 controlValve.openValveStartCycle()
 
 #This function schedules itself
